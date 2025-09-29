a5ee4a8d04bf1a5b5d7f49758c4ad366b7730463
Arfah Aadil <your_github_arfahaadil@gmail.com>
2025-09-29 09:08:18 +0100

    Updated CSS of Portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,20 +5,20 @@ function About() {
     <section className="about" id="about">
       <h2>About Me</h2>
       <p>
-        Hi, I’m Arfah Aadil, an MSc Software Engineering graduate with a passion for building scalable, secure, 
-        and efficient software solutions. My technical skills spans in full-stack development, from building robust 
-        backend systems with Java, as well as creating responsive and user-friendly frontend interfaces using 
-        HTML, CSS, Bootstrap, and React. I also have experience designing and deploying cloud-native applications 
-        on platforms like GCP. I have hands-on experience with microservices architecture, REST APIs, Docker, 
-        Kubernetes, and CI/CD pipelines, enabling me to develop high-performance systems that meet business and
-        user needs.
+        Hi, I’m Arfah Aadil, an MSc Software Engineering graduate with experience in Java, Spring Boot, React, 
+        and cloud application development. I specialize in building scalable backend systems, secure REST APIs, 
+        and responsive frontends, with hands-on experience in Docker, Kubernetes, cloud platforms, and CI/CD 
+        pipelines.
       </p>
       <p>
-       I enjoy collaborating in Agile teams, solving complex problems, and continuously learning new technologies 
-       to enhance my skill set. Throughout my projects and internships, I’ve contributed to enterprise internal 
-       platforms, data-driven systems, and automation tools, always striving to deliver impactful solutions. 
-       I’m eager to take on challenging software engineering roles where I can apply my technical skills 
-       and creativity to drive meaningful results.
+       I have a passion for creating efficient, user-focused software solutions and thrive in Agile environments. 
+       Through my internships, freelance work, and academic projects, I’ve contributed to enterprise platforms, 
+       automation tools, and data-driven systems, always aiming to deliver impactful and high-performance 
+       applications.
+      </p>
+      <p>
+       I’m currently seeking Software Engineering opportunities with a focus on full-stack development and cloud 
+       applications, where I can apply my skills, grow professionally, and contribute to meaningful projects.
       </p>
     </section>
   );
